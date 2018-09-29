68799b66d79cc738716f55ef66a50749a93aa76c
Emmet Delaney <emmetdel@gmail.com>
2018-09-29 08:50:06 +0100

    added webpack config files

---
+++ sw-precache-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  globPatterns: [
+    'manifest.json',
+    'src/**/*',
+  ]
+};
---
+++ webpack.config.js
@@ -0,0 +1,129 @@
+'use strict';
+
+const { resolve, join } = require('path');
+const merge = require('webpack-merge');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {GenerateSW} = require('workbox-webpack-plugin');
+const serviceWorkerConfig = require('./sw-precache-config.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const ENV = process.argv.find(arg => arg.includes('production'))
+  ? 'production'
+  : 'development';
+const OUTPUT_PATH = ENV === 'production' ? resolve('dist') : resolve('src');
+const INDEX_TEMPLATE = resolve('./index.html');
+
+const webcomponentsjs = './node_modules/@webcomponents/webcomponentsjs';
+
+const assets = [
+  {
+    from: resolve('./images/favicon.ico'),
+    to: resolve('dist/assets/')
+  },
+  {
+    from: resolve('./manifest.json'),
+    to: resolve('dist/')
+  },
+  {
+    from: resolve('./images/manifest/icon-192x192.png'),
+    to: resolve('dist/assets')
+  },
+];
+
+const polyfills = [
+  {
+    from: resolve(`${webcomponentsjs}/webcomponents-*.js`),
+    to: join(OUTPUT_PATH, 'vendor'),
+    flatten: true
+  },
+  {
+    from: resolve(`${webcomponentsjs}/bundles/*.js`),
+    to: join(OUTPUT_PATH, 'vendor', 'bundles'),
+    flatten: true
+  },
+  {
+    from: resolve(`${webcomponentsjs}/custom-elements-es5-adapter.js`),
+    to: join(OUTPUT_PATH, 'vendor'),
+    flatten: true
+  },
+  {
+    from: resolve('./node_modules/promise-polyfill/dist/polyfill.min.js'),
+    to: join(OUTPUT_PATH, 'vendor')
+  }
+];
+
+const commonConfig = merge([
+  {
+    entry: './src/my-app.ts',
+    devtool: 'inline-source-map',
+    output: {
+      path: OUTPUT_PATH,
+      filename: '[name].[chunkhash:8].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    },
+  }
+]);
+
+const developmentConfig = merge([
+  {
+    devtool: 'cheap-module-source-map',
+    plugins: [
+      new CopyWebpackPlugin(polyfills),
+      new HtmlWebpackPlugin({
+        template: INDEX_TEMPLATE
+      }),
+      // new BundleAnalyzerPlugin()
+    ],
+
+    devServer: {
+      contentBase: OUTPUT_PATH,
+      compress: true,
+      overlay: true,
+      port: 3000,
+      historyApiFallback: true,
+      host: 'localhost'
+    }
+  }
+]);
+
+const productionConfig = merge([
+  {
+    devtool: 'nosources-source-map',
+    plugins: [
+      new CleanWebpackPlugin([OUTPUT_PATH], { verbose: true }),
+      new CopyWebpackPlugin([...polyfills, ...assets]),
+      new HtmlWebpackPlugin({
+        template: INDEX_TEMPLATE,
+        minify: {
+          collapseWhitespace: true,
+          removeComments: true,
+          minifyCSS: true,
+          minifyJS: true
+        }
+      }),
+      new GenerateSW(serviceWorkerConfig),
+      // new BundleAnalyzerPlugin()
+    ]
+  }
+]);
+
+module.exports = mode => {
+  if (mode === 'production') {
+    return merge(commonConfig, productionConfig, { mode });
+  }
+
+  return merge(commonConfig, developmentConfig, { mode });
+};
